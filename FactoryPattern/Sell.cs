27ee4f2479baf4acaf434d3f53bac5b9853b7766
TaylanCann <taylancanh@gmail.com>
2022-04-14 01:34:21 +0300

    Abstract sell class created

---
+++ FactoryPattern/Sell.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    public abstract class Sell
+    {
+        public abstract void Product();
+    }
+
+    public class Computer : Sell
+    {
+        public override void Product()
+        {
+            Console.WriteLine("Computer sold.");
+        }
+    }
+
+    public class Apple : Sell
+    {
+        public override void Product()
+        {
+            Console.WriteLine("Apple sold.");
+        }
+    }
+}
